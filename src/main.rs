447b7d47f8eb8fa0ed7c810dcf54a0bb28b8f966
thiagopls1 <thiagopls1@hotmail.com>
2026-04-20 19:27:09 -0300

    feat: add source code path handling

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,25 @@
-use std::env;
+use std::{env, fs, io::ErrorKind, process};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+    let src_code_path = env::args().nth(1).unwrap_or_else(|| {
+        eprintln!("ERRO: É necessário passar o caminho do arquivo de código-fonte!");
+        process::exit(1);
+    });
+
+    let ndr_code = match fs::read_to_string(&src_code_path) {
+        Ok(content) => content,
+        Err(err) => match err.kind() {
+            ErrorKind::NotFound => {
+                eprintln!("ERRO: Arquivo não encontrado!");
+                process::exit(1);
+            }
+            _ => {
+                eprintln!("ERRO: Não foi possível ler o arquivo!");
+                process::exit(1);
+            }
+        },
+    };
+
+    println!("{}", ndr_code);
+    process::exit(0);
 }
